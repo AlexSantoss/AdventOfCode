eba52b466bf015cc67266dfcdf744c32409ceb1d
Alex de Oliveira <asobrasil@yahoo.com.br>
2021-12-02 17:24:53 -0300

    *Day2

--- 2021/day2.py
+++ 2021/day2.py
@@ -2,22 +2,17 @@ from helper import readStdin
 commands = [line.split() for line in readStdin()]
 
 #part 1
-horizontal = 0
-depth = 0
+depth = horizontal = 0
 for c in commands:
     if c[0] == 'forward': horizontal += int(c[1])
-    elif c[0] == 'down': depth +=  int(c[1])
-    elif c[0] == 'up': depth -= int(c[1])
+    else: depth += int(c[1]) if c[0] == 'down' else -int(c[1])
 print(horizontal * depth)
 
 #part 2
-horizontal = 0
-depth = 0
-aim = 0
+aim = depth = horizontal = 0
 for c in commands:
     if c[0] == 'forward':
         horizontal += int(c[1])
         depth += aim*int(c[1])
-    elif c[0] == 'down': aim +=  int(c[1])
-    elif c[0] == 'up': aim -= int(c[1])
+    else: aim += int(c[1]) if c[0] == 'down' else -int(c[1])
 print(horizontal * depth)
